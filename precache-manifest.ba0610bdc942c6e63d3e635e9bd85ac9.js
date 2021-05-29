f86c70a58fdd2a87780116d83204493b08f832f9
VilenaArturovna <vilena-forever@inbox.ru>
2021-05-30 01:17:29 +0600

    Updates

---
+++ precache-manifest.ba0610bdc942c6e63d3e635e9bd85ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ed17eda405079c9aa23159252774b3",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e0d26b837384919d7df8",
+    "url": "/todolist/static/css/main.fa6bf411.chunk.css"
+  },
+  {
+    "revision": "7d7eb21240bf435281bd",
+    "url": "/todolist/static/js/2.115bc8e8.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.115bc8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d26b837384919d7df8",
+    "url": "/todolist/static/js/main.438fb2a5.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
